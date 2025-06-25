fbf099650df3585420cff9ee390246e8574624ed
Kendall Hecht <kendallhecht@gmail.com>
2025-06-25 12:58:20 -0400

    hopefully fixing multi-page website deployment

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// export default {
+//     // config options
+//   }
+
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gardenPlanner: resolve(__dirname, 'garden-planner/index.html'),
+        blog: resolve(__dirname, 'blog/index.html'),
+      },
+    },
+  },
+})
